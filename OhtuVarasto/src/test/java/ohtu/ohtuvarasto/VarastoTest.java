19d9194e92b17c494ec8977f2d3f397c78f9f9e6
Leo Pynnonen <hifk_pyntsi@hotmail.com>
2015-03-14 19:28:00 +0200

    Rikottu

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -59,7 +59,7 @@ public class VarastoTest {
     public void ottaminenLisääTilaa() {
         varasto.lisaaVarastoon(8);
 
-        varasto.otaVarastosta(2);
+        varasto.otaVarastosta(3);
 
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
